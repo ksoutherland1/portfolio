97e8af806c7fbcf510db6ea413cfa02b69a23878
ksouth <katesoutherland8@gmail.com>
2022-06-28 07:10:36 -0700

    Debugged mail event

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const resume = document.querySelector("#resume")
-
-resume.addEventListener('click, ')
+const sendMail = document.querySelector("#mail").addEventListener('click', function runSendMail(){
+    let email="katesoutherland8@gmail.com";
+    location.href = "mailto:"+email
+})
