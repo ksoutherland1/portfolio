72a6a23766919b0ef990e0aa2709a544230c5199
ksouth <katesoutherland8@gmail.com>
2022-06-28 09:26:03 -0700

    Event listener added to resume

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const sendMail = document.querySelector("#mail").addEventListener('click', funct
     let email="katesoutherland8@gmail.com";
     location.href = "mailto:"+email
 })
+
+const window = document.querySelector("#resume").addEventListener("click", function openPdf(){
+    window.open("Here Download PDF url", '_blank');
+})
